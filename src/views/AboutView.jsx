9135a7f528919c896dea29d882f56ab72c6c82c4
JaxBBLL <showcye@foxmail.com>
2022-04-20 09:22:58 +0800

    fix

--- src/views/AboutView.jsx
+++ src/views/AboutView.jsx
@@ -1,13 +1,12 @@
 import { defineComponent, ref } from 'vue'
 import { NButton } from 'naive-ui'
 
-const msg = ref('abc')
-const handleClick = () => {
-  msg.value = '123'
-}
-
 export default defineComponent({
   render() {
+    const msg = ref('abc')
+    const handleClick = () => {
+      msg.value = '123'
+    }
     return (
       <NButton type="primary" onClick={handleClick}>
         click me {msg.value}
